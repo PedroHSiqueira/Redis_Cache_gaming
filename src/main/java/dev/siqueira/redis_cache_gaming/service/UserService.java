75693579ffc30b43c3766fcb6de5daccc3f0d06f
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2026-03-19 11:02:45 -0300

    Feat: Adição do redis template e salvamento inicial do ranking no redis feito,

--- src/main/java/dev/siqueira/redis_cache_gaming/service/UserService.java
+++ src/main/java/dev/siqueira/redis_cache_gaming/service/UserService.java
@@ -5,20 +5,25 @@ import dev.siqueira.redis_cache_gaming.entity.User;
 import dev.siqueira.redis_cache_gaming.repository.UserRepository;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class UserService {
 
-    private UserRepository  userRepository;
+    private final UserRepository  userRepository;
+    private final RedisTemplate<String, String> redisTemplate;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, RedisTemplate<String, String> redisTemplate) {
         this.userRepository = userRepository;
+        this.redisTemplate = redisTemplate;
     }
 
     public User save(User user){
-        return userRepository.save(user);
+        User userDB = userRepository.save(user);
+        redisTemplate.opsForZSet().add("ranking", userDB.getId().toString(), userDB.getPoints());
+        return userDB;
     }
 
     @Cacheable(value = "users", key = "#id", unless = "#result == null")
